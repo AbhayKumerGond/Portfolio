c7b8f4c828d307e764d9f3144149e33e02bcd5a0
AbhayKumerGond <kumargondabhay@gmail.com>
2024-11-26 18:10:39 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,327 @@
+$(document).ready (function(){
+  $("#bb").mouseover(function(){
+   $("#aa").css({"background-color" : "white","transition":".5s",});
+  });
+	$("#bb").mouseout(function(){
+   $("#aa").css({"background-color" : "black","transition":".5s",});
+	});
+
+	$("#bb").mouseout(function(){
+   $("#cc").css({"color":"white","background-color":"grey",});
+	});
+	$("#bb").mouseover(function(){
+   $("#cc").css({"color":"white","background-color":"#D1C12F","transform":"0.5s"}); 
+	});
+	
+	$("#bb").mouseout(function(){
+   $(".gg").css({"color":"white",});
+	});
+	$("#bb").mouseover(function(){
+   $(".gg").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#bb").mouseout(function(){
+   $(".ff").css({"color":"white"});
+	});
+	$("#bb").mouseover(function(){
+   $(".ff").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#bb").mouseout(function(){
+   $(".kk").css({"color":"white"});
+	});
+	$("#bb").mouseover(function(){
+   $(".kk").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	
+	$("#aa").mouseover(function(){
+   $("#aa").css({"background-color" : "white","transition":".5s",});
+  });
+	$("#aa").mouseout(function(){
+   $("#aa").css({"background-color" : "black","transition":".5s",});
+	});
+	
+	$("#aa").mouseout(function(){
+   $("#cc").css({"color":"white","background-color":"grey",});
+	});
+	$("#aa").mouseover(function(){
+   $("#cc").css({"color":"white","background-color":"#D1C12F","transform":"0.5s"}); 
+	});
+	
+	$("#aa").mouseout(function(){
+   $(".gg").css({"color":"white",});
+	});
+	$("#aa").mouseover(function(){
+   $(".gg").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#aa").mouseout(function(){
+   $(".ff").css({"color":"white"});
+	});
+	$("#aa").mouseover(function(){
+   $(".ff").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#aa").mouseout(function(){
+   $(".kk").css({"color":"white"});
+	});
+	$("#aa").mouseover(function(){
+   $(".kk").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	
+	$("#dd").mouseover(function(){
+   $("#zz").css({"background-color" : "white","transition":".5s",});
+  });
+	$("#dd").mouseout(function(){
+   $("#zz").css({"background-color" : "black","transition":".5s",});
+	});
+
+	$("#dd").mouseout(function(){
+   $("#ll").css({"color":"white","background-color":"grey",});
+	});
+	$("#dd").mouseover(function(){
+   $("#ll").css({"color":"white","background-color":"#D1C12F","transform":"0.5s"}); 
+	});
+	
+	$("#dd").mouseout(function(){
+   $("#nn").css({"color":"white",});
+	});
+	$("#dd").mouseover(function(){
+   $("#nn").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#dd").mouseout(function(){
+   $(".tt").css({"color":"white"});
+	});
+	$("#dd").mouseover(function(){
+   $(".tt").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#dd").mouseout(function(){
+   $("#mm").css({"color":"white"});
+	});
+	$("#dd").mouseover(function(){
+   $("#mm").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+
+
+$("#zz").mouseover(function(){
+   $("#zz").css({"background-color" : "white","transition":".5s",});
+  });
+	$("#zz").mouseout(function(){
+   $("#zz").css({"background-color" : "black","transition":".5s",});
+	});
+	
+	$("#zz").mouseout(function(){
+   $("#ll").css({"color":"white","background-color":"grey",});
+	});
+	$("#zz").mouseover(function(){
+   $("#ll").css({"color":"white","background-color":"#D1C12F","transform":"0.5s"}); 
+	});
+	
+	$("#zz").mouseout(function(){
+   $("#nn").css({"color":"white",});
+	});
+	$("#zz").mouseover(function(){
+   $("#nn").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#zz").mouseout(function(){
+   $(".tt").css({"color":"white"});
+	});
+	$("#zz").mouseover(function(){
+   $(".tt").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#zz").mouseout(function(){
+   $("#mm").css({"color":"white"});
+	});
+	$("#zz").mouseover(function(){
+   $("#mm").css({"color":"black","transform":"0.5s"}); 
+	});
+
+
+ $("#vv").mouseover(function(){
+   $("#oo").css({"background-color" : "white","transition":".5s",});
+  });
+	$("#vv").mouseout(function(){
+   $("#oo").css({"background-color" : "black","transition":".5s",});
+	});
+
+	$("#vv").mouseout(function(){
+   $("#ww").css({"color":"white","background-color":"grey",});
+	});
+	$("#vv").mouseover(function(){
+   $("#ww").css({"color":"white","background-color":"#D1C12F","transform":"0.5s"}); 
+	});
+	
+	$("#vv").mouseout(function(){
+   $("#rr").css({"color":"white",});
+	});
+	$("#vv").mouseover(function(){
+   $("#rr").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#vv").mouseout(function(){
+   $(".pp").css({"color":"white"});
+	});
+	$("#vv").mouseover(function(){
+   $(".pp").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#vv").mouseout(function(){
+   $("#ee").css({"color":"white"});
+	});
+	$("#vv").mouseover(function(){
+   $("#ee").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#oo").mouseover(function(){
+   $("#oo").css({"background-color" : "white","transition":".5s",});
+  });
+	$("#oo").mouseout(function(){
+   $("#oo").css({"background-color" : "black","transition":".5s",});
+	});
+
+	$("#oo").mouseout(function(){
+   $("#ww").css({"color":"white","background-color":"grey",});
+	});
+	$("#oo").mouseover(function(){
+   $("#ww").css({"color":"white","background-color":"#D1C12F","transform":"0.5s"}); 
+	});
+	
+	$("#oo").mouseout(function(){
+   $("#rr").css({"color":"white",});
+	});
+	$("#oo").mouseover(function(){
+   $("#rr").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#oo").mouseout(function(){
+   $(".pp").css({"color":"white"});
+	});
+	$("#oo").mouseover(function(){
+   $(".pp").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	$("#oo").mouseout(function(){
+   $("#ee").css({"color":"white"});
+	});
+	$("#oo").mouseover(function(){
+   $("#ee").css({"color":"black","transform":"0.5s"}); 
+	});
+	
+	
+	
+	
+	
+	});
+
+	$(document).ready (function(){
+	$("#al").mouseover(function(){
+		$("#bl").addClass("showw");
+	});
+	$("#al").mouseout(function(){
+		$("#bl").removeClass("showw");
+	
+	});
+		$("#fl").mouseover(function(){
+		$("#jl").addClass("showw");
+	});
+	$("#fl").mouseout(function(){
+		$("#jl").removeClass("showw");
+	
+	});
+		
+		$("#ac").mouseover(function(){
+		$("#ng").addClass("showw");
+	});
+	$("#ac").mouseout(function(){
+		$("#ng").removeClass("showw");
+	
+	});
+		
+		$("#bm").mouseover(function(){
+		$("#kl").addClass("showw");
+	});
+	$("#bm").mouseout(function(){
+		$("#kl").removeClass("showw");
+	
+	});
+		$("#qr").mouseover(function(){
+		$("#st").addClass("showw");
+	});
+	$("#qr").mouseout(function(){
+		$("#st").removeClass("showw");
+	
+	});
+		
+		$("#mn").mouseover(function(){
+		$("#op").addClass("showw");
+	});
+	$("#mn").mouseout(function(){
+		$("#op").removeClass("showw");
+	
+	});
+	
+	$("#sc").mouseover(function(){
+		$("#gs").css({"top":"500px","transition":"0.5s"});
+	});
+	$("#sc").mouseout(function(){
+		$("#gs").css({"top":"755px",});
+	});
+		
+		$("#gc").mouseover(function(){
+		$("#fs").css({"top":"500px","transition":"0.5s"});
+	});
+	$("#gc").mouseout(function(){
+		$("#fs").css({"top":"755px",});
+	});
+		
+		$("#vc").mouseover(function(){
+		$("#lc").css({"top":"500px","transition":"0.5s"});
+	});
+	$("#vc").mouseout(function(){
+		$("#lc").css({"top":"755px",});
+	});
+		
+		$("#down").mouseover(function(){
+		$("#scroll").css({"top":"500px","transition":"0.5s"});
+	});
+	$("#down").mouseout(function(){
+		$("#scroll").css({"top":"755px",});
+	});
+		
+		$("#cool").mouseover(function(){
+		$("#ward").css({"top":"500px","transition":"0.5s"});
+	});
+	$("#cool").mouseout(function(){
+		$("#ward").css({"top":"755px",});
+	});
+		$("#charm").mouseover(function(){
+		$("#clear").css({"top":"500px","transition":"0.5s"});
+	});
+	$("#charm").mouseout(function(){
+		$("#clear").css({"top":"755px",});
+	});
+		
+		$("#happen").mouseover(function(){
+		$("#good").css({"top":"500px","transition":"0.5s"});
+	});
+	$("#happen").mouseout(function(){
+		$("#good").css({"top":"755px",});
+	});
+		
+		$("#well").mouseover(function(){
+		$("#sword").css({"top":"500px","transition":"0.5s"});
+	});
+	$("#well").mouseout(function(){
+		$("#sword").css({"top":"755px",});
+	});
+
+
+});
+	
